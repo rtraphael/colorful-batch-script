37aa9b17d4f21c9bf2d313bbc79fcadce021b042
Raphaël <41703575+rtraphael@users.noreply.github.com>
2021-12-21 15:31:11 +0100

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,12 +1,2 @@
-@ECHO OFF
-
-:start
-for %%a in (0 1 2 3 4 5 6 7 8 9 A B C D E F) do (
-  for %%b in (F E D C B A 9 8 7 6 5 4 3 2 1 0) do (
-    color %%b%%a
-  )
-)
-
+for %%c in (0, 1, 100) do (for %%a in (0 1 2 3 4 5 6 7 8 9 A B C D E F) do (for %%b in (F E D C B A 9 8 7 6 5 4 3 2 1 0) do (color %%b%%a))
 color 00
-
-GOTO :start
